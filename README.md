4d4a5416da918cb161890026eeefa155e74d468f
Denise Case <denisecase@gmail.com>
2019-08-10 21:25:52 -0500

    moved assets to folders

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # Applied Physics Engineering
 
-> Web
+> This is the updated website for Applied Physics Engineering
+
+- Bruce Johnson
+- White Bear Lake, MN
 
 ## Links
 
 * Site: <https://denisecase.github.io/ape>
 * Source: <https://github.com/denisecase/ape>
 
+## Recommended
 
+- [Git](https://git-scm.com/download/win) version control system
+- [TortoiseGit](https://tortoisegit.org/) - integrates with Windows File Explorer
+- [Firefox](https://www.mozilla.org/en-US/firefox/) web browser or
+- [Chrome](https://www.google.com/chrome/) web browser
+- [Visual Studio Code](https://code.visualstudio.com/) - editor
+- [Tables Generator (HTML)](https://www.tablesgenerator.com/html_tables)
+- [Google Analytics](https://analytics.google.com/analytics/web/)
+- [Google Workbox](https://developers.google.com/web/tools/workbox/)
+- [JsDoc Cheatsheet](https://devhints.io/jsdoc)
+- [Test JavaScript Regular Expressions](https://regexr.com/)
+- [Prettier](https://prettier.io/)
+- [ESLint](https://eslint.org/)
+- [Favicon.io](https://favicon.io)
+- [PWA Builder App Image Generator](https://www.pwabuilder.com/imageGenerator)
+
+## Getting Started
+
+- Create a free [GitHub](https://github.com/) account.
+- Fork this repository into a new repository in your GitHub account.
+- Clone your repository down to your local machine (or edit it directly 'in the cloud').
+- Open GitHub repository, under Settings / GitHub Pages, set Source to master branch and click Save.
+- Under Settings / Collaborators, add account names (e.g. denisecase and profcase) and invite.
+- Go to your Google Analytics dashboard. Add a new property for your GitHub Page, generate your tracking id, and replace the analytics code in index.html with yours.
 
 
---
+++ service-worker.js
@@ -0,0 +1,158 @@
+/**
+ *  @fileOverview Provides an asynchronous service worker to manage application behavior.
+ *
+ * Service workers are capable of intercepting and adjusting all requests
+ * before they are sent and after they return.
+ *
+ * Services workers can manage caching files to improve performance,
+ * provide offline app experiences, enable 'home page' installation on devices,
+ * and provide push notifications to your users.
+ *
+ * An updated Service worker will download (upon first page access, or every 24 hours).
+ * If new, it will yield the install event.
+ * It will yield the activate event after pages load and old service worker is no longer used.
+ *
+ * Use Chrome Dev Tools / Application to clear storage.
+ *
+ * Use Chrome Dev Tools / Application / ServiceWorker to debug.
+ * Check "update on reload" to force service worker update on page reload.
+ *
+ * Use Chrome Dev Tools / Audit to evaluate.
+ *
+ * JSDoc comments are written in Markdown.
+ *
+ * @author       Denise Case
+ *
+ * @requires     EXTERNAL:@link{https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js}
+ */
+
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+)
+
+if (workbox) {
+  console.log('Service worker Workbox loaded', workbox.routing)
+
+  const appName = 'Applied Physics Engineering'
+  const appVersion = 'v1'
+  const maxAgeDay = 1 * 24 * 60 * 60
+  // eslint-disable-next-line no-unused-vars
+  const httpResponseOpaque = 0 // CORS
+  const httpReponseOk = 200 // good
+
+  // test Regular Expressions at https://regexr.com/
+  const reStatic = /\.(?:js|css)$/
+  const reImages = /\.(?:png|gif|jpg|jpeg|webp|svg)$/
+  const reCdnFont = /https:\/\/use\.fontawesome\.com\/.*all\.css$/
+  const reCdnStyles = /https:\/\/cdnjs\.cloudflare\.com\/.*\.css$/
+
+  workbox.core.setCacheNameDetails({
+    prefix: appName,
+    suffix: appVersion,
+    precache: 'custom-precache-name',
+    runtime: 'custom-runtime-name'
+  })
+
+  const precacheCacheName = workbox.core.cacheNames.precache
+  const runtimeCacheName = workbox.core.cacheNames.runtime
+
+  console.log(`precacheCacheName=${precacheCacheName}`)
+  console.log(`runtimeCacheName=${runtimeCacheName}`)
+
+  // use stale cached cdn font files while downloading new
+
+  workbox.routing.registerRoute(
+    reCdnFont,
+    new workbox.strategies.StaleWhileRevalidate()
+  )
+
+  // use stale cached cdn style files while downloading new
+
+  workbox.routing.registerRoute(
+    reCdnStyles,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: `${appName}-cdn-css`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 90,
+          maxAgeSeconds: maxAgeDay,
+          purgeOnQuotaError: true
+        }),
+        new workbox.cacheableResponse.Plugin({
+          statuses: [httpReponseOk]
+        })
+      ]
+    })
+  )
+
+  // Use stale local static files (js/css) while downloading new
+
+  workbox.routing.registerRoute(
+    reStatic,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: `${appName}-static-css-js`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 90,
+          maxAgeSeconds: maxAgeDay,
+          purgeOnQuotaError: true
+        })
+      ]
+    })
+  )
+
+  // Fetch images, try local cache first
+
+  workbox.routing.registerRoute(
+    reImages,
+    new workbox.strategies.CacheFirst({
+      cacheName: `${appName}-images`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: maxAgeDay,
+          purgeOnQuotaError: true
+        })
+      ]
+    })
+  )
+
+  // Define a common handler if any of the fetching methods fail
+
+  workbox.routing.setCatchHandler(({ event }) => {
+    console.error(`Error: ${event.error}`)
+    if (event.request.mode === 'navigate') {
+      return caches.match('/error-page.html')
+    }
+    return Response.error()
+  })
+
+  // respond with 200 (ok) even when offline
+
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(`${appName}-static`).then(cache => {
+        return cache.addAll([
+          '.',
+          'index.html',
+          'multimotionswing.html',
+          'sketchingdemo.html,
+          'bouncelessstop.html',
+          'kakurohints.html'
+        ])
+      })
+    )
+  })
+
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+    )
+  })
+}
